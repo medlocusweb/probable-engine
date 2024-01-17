37e8c4f847aee814b3886727781761d83fbf1325
sajid <2mdsajid@gmail.com>
2024-01-18 01:00:39 +0545

    2nd commit in fork

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const About = (props: Props) => {
+    return (
+        <div>
+            <p>About</p>
+            <p>hi this is me sajid</p>
+        </div>
+    )
+}
+
+export default About
