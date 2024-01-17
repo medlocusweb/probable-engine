85c5f46260116db6784b92377a774ba3015ecefa
sajid <2mdsajid@gmail.com>
2024-01-18 01:05:24 +0545

    3nd commit in fork

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <p>hello peter !</p>
-     <p>yeah again this is me</p>
+     <p>yeah again this is 2nd time me</p>
     </main>
   )
 }
